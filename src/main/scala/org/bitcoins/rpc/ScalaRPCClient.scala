fbb8881e3df04fa68304f748946c61fb9933c6d9
Chris Stewart <stewart.chris1234@gmail.com>
2017-04-29 23:37:06 -0500

    Refactoring all rpc calls to go through rpc interface instead of directly sent to the command line

--- src/main/scala/org/bitcoins/rpc/ScalaRPCClient.scala
+++ src/main/scala/org/bitcoins/rpc/ScalaRPCClient.scala
@@ -1,10 +1,12 @@
 package org.bitcoins.rpc
 
 import akka.actor.ActorSystem
-import akka.http.scaladsl.model.Uri
+import akka.http.scaladsl.model.{HttpEntity, Uri}
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Sink
+import akka.util.ByteString
 import org.bitcoins.core.config.{MainNet, NetworkParameters, RegTest}
-import org.bitcoins.core.crypto.ECPrivateKey
+import org.bitcoins.core.crypto.{DoubleSha256Digest, ECPrivateKey}
 import org.bitcoins.core.currency.{CurrencyUnit, CurrencyUnits, Satoshis}
 import org.bitcoins.core.number.Int64
 import org.bitcoins.rpc.marshallers.RPCMarshallerUtil
@@ -24,40 +26,63 @@ import spray.json._
 import org.bitcoins.rpc.marshallers.wallet.UTXOMarshaller._
 
 import scala.concurrent.Future
+import scala.concurrent.duration.DurationInt
 import scala.sys.process._
 
 /**
   * Created by Tom on 1/14/2016.
   */
-class ScalaRPCClient(instance: BitcoindInstance, username: String, password: String, system: ActorSystem,
+class ScalaRPCClient(instance: BitcoindInstance, username: String, password: String, materializer: ActorMaterializer,
                      datadir: String = System.getProperty("user.home") + "/.bitcoin") extends RPCMarshallerUtil
   with BitcoinSLogger with DefaultJsonProtocol {
 
-
+  implicit val dispatcher = materializer.system.dispatcher
   /**
    * Refer to this reference for list of RPCs
    * https://bitcoin.org/en/developer-reference#rpcs */
-  def sendCommand(command : String) : String = {
-    val networkArg = parseNetworkArg
-    val cmd = "bitcoin-cli " + networkArg + " -datadir=" + datadir + " " + command
-    val result = cmd.!!
-    result
+  def sendCommand(command : String) : Future[JsObject] = {
+    val request = RPCHandler.buildRequest(command)
+    val result = RPCHandler.sendRequest(username,password,instance,request)
+    val source: Future[HttpEntity.Strict] = result.flatMap(_.entity.toStrict(5.seconds)(materializer))
+    source.map { ent =>
+      val decoded = ent.data.decodeString(ByteString.UTF_8)
+      decoded.parseJson.asJsObject
+    }
+  }
+
+  def sendCommand(command: String, arg: Int): Future[JsObject] = {
+    val request = RPCHandler.buildRequest(command,arg)
+    val result = RPCHandler.sendRequest(username,password,instance,request)
+    val source: Future[HttpEntity.Strict] = result.flatMap(_.entity.toStrict(5.seconds)(materializer))
+    source.map(_.data.decodeString(ByteString.UTF_8).parseJson.asJsObject)
+  }
+
+  def sendCommand(command: String, arg: String): Future[JsObject] = {
+    val request = RPCHandler.buildRequest(command,arg)
+    val result = RPCHandler.sendRequest(username,password,instance,request)
+    val source: Future[HttpEntity.Strict] = result.flatMap(_.entity.toStrict(5.seconds)(materializer))
+    source.map(_.data.decodeString(ByteString.UTF_8).parseJson.asJsObject)
+  }
+
+  def sendCommand(command: String, arg: JsArray): Future[JsObject] = {
+    val request = RPCHandler.buildRequest(command,arg)
+    val result = RPCHandler.sendRequest(username,password,instance,request)
+    val source: Future[HttpEntity.Strict] = result.flatMap(_.entity.toStrict(5.seconds)(materializer))
+    source.map(_.data.decodeString(ByteString.UTF_8).parseJson.asJsObject)
   }
 
   /** Starts the bitcoind instance */
-  def start: String = {
+  def start: Unit = {
     val networkArg = parseNetworkArg
     val cmd = "bitcoind " + networkArg + " -datadir=" + datadir + " -daemon"
-    val result = cmd.!!
-    result
+    cmd.!!
   }
 
 
   /** This will stop the server */
   def stop: Future[Unit] = {
-    val json = RPCHandler.buildRequest("stop")
-    val response = RPCHandler.sendRequest(username,password,instance,json)
-    response.map(_ => ())(system.dispatcher)
+    val response = sendCommand("stop")
+    response.map(_ => ())
   }
 
   /**
@@ -65,29 +90,38 @@ class ScalaRPCClient(instance: BitcoindInstance, username: String, password: Str
    * this number will be 0
    * https://bitcoin.org/en/developer-reference#getblockcount
    */
-  def getBlockCount : Int = sendCommand("getblockcount").trim.toInt
+  def getBlockCount : Future[Int] = {
+    sendCommand("getblockcount").map(json => json.fields("result").convertTo[Int])
+  }
 
   /** Generates an arbitrary number of blocks in regtest */
-  def generate(num: Int): String = {
+  def generate(num: Int): Future[Seq[DoubleSha256Digest]] = {
     require(instance.network == RegTest, "Can only generate blocks in regtest")
-    sendCommand("generate " + num)
+    val result = sendCommand("generate",num)
+    result.map { r =>
+      r.fields("result") match {
+        case arr: JsArray =>
+          arr.elements.map(hash => DoubleSha256Digest(hash.toString()))
+        case x @ _ => throw new IllegalArgumentException("Expected JsArray from bitcoin-cli generate, got: " + x)
+      }
+    }
   }
   /**
    * The getmempoolinfo RPC returns information about the node's current transaction memory pool.
    * https://bitcoin.org/en/developer-reference#getmempoolinfo
    */
-  def getMemPoolInfo : MemPoolInfo = {
-    val result : String = sendCommand("getmempoolinfo")
-    MemPoolInfoMarshaller.MemPoolInfoFormatter.read(result.parseJson)
+  def getMemPoolInfo: Future[MemPoolInfo] = {
+    val result = sendCommand("getmempoolinfo")
+    result.map(r => MemPoolInfoMarshaller.MemPoolInfoFormatter.read(r))
   }
 
   /**
    * Information about the current state of the local block chain.
    * https://bitcoin.org/en/developer-reference#getblockchaininfo
    */
-  def getBlockChainInfo : BlockchainInfo = {
-    val result : String = sendCommand("getblockchaininfo")
-    BlockchainInfoRPCMarshaller.BlockchainInfoFormatter.read(result.parseJson)
+  def getBlockChainInfo: Future[BlockchainInfo] = {
+    val result = sendCommand("getblockchaininfo")
+    result.map(r => BlockchainInfoRPCMarshaller.BlockchainInfoFormatter.read(r))
   }
 
   /**
@@ -96,46 +130,46 @@ class ScalaRPCClient(instance: BitcoindInstance, username: String, password: Str
    * not count outputs from the memory pool.
    * https://bitcoin.org/en/developer-reference#gettxoutsetinfo
    */
-  def getTxOutSetInfo : ConfirmedUnspentTransactionOutput = {
-    val result : String = sendCommand("gettxoutsetinfo")
-    ConfirmedUnspentTransactionOutputMarshaller.ConfirmedUnspentTransactionOutputFormatter.read(result.parseJson)
+  def getTxOutSetInfo: Future[ConfirmedUnspentTransactionOutput] = {
+    val result = sendCommand("gettxoutsetinfo")
+    result.map(r =>
+      ConfirmedUnspentTransactionOutputMarshaller.ConfirmedUnspentTransactionOutputFormatter.read(r))
   }
 
   /**
    * The getmininginfo RPC returns various mining-related information.
    * https://bitcoin.org/en/developer-reference#getmininginfo
    */
-  def getMiningInfo : GetMiningInfo = {
-    val result : String = sendCommand("getmininginfo")
-    MiningInfoMarshaller.MiningInfoFormatter.read(result.parseJson)
+  def getMiningInfo: Future[GetMiningInfo] = {
+    val result = sendCommand("getmininginfo")
+    result.map(r => MiningInfoMarshaller.MiningInfoFormatter.read(r))
   }
 
   /**
    * The getnetworkinfo RPC returns information about the node’s connection to the network.
    * https://bitcoin.org/en/developer-reference#getnetworkinfo
    */
-  def getNetworkInfo : NetworkInfo = {
-    val result : String = sendCommand("getnetworkinfo")
-    NetworkRPCMarshaller.NetworkInfoFormatter.read(result.parseJson)
+  def getNetworkInfo: Future[NetworkInfo] = {
+    val result = sendCommand("getnetworkinfo")
+    result.map(r => NetworkRPCMarshaller.NetworkInfoFormatter.read(r))
   }
 
   /**
    * The getpeerinfo RPC returns data about each connected network node.
    * https://bitcoin.org/en/developer-reference#getpeerinfo
    */
-  def getPeerInfo : Seq[PeerInfo] = {
-    val result : String = sendCommand("getpeerinfo")
-    val json = result.parseJson
-    convertToPeerInfoSeq(json)
+  def getPeerInfo : Future[Seq[PeerInfo]] = {
+    val result = sendCommand("getpeerinfo")
+    result.map(r => convertToPeerInfoSeq(r))
   }
 
   /**
    * The getwalletinfo RPC provides information about the wallet.
    * https://bitcoin.org/en/developer-reference#getwalletinfo
    */
-  def getWalletInfo : WalletInfo = {
-    val result : String = sendCommand("getwalletinfo")
-    WalletMarshaller.WalletFormatter.read(result.parseJson)
+  def getWalletInfo: Future[WalletInfo] = {
+    val result = sendCommand("getwalletinfo")
+    result.map(r => WalletMarshaller.WalletFormatter.read(r))
   }
 
   /**
@@ -143,101 +177,108 @@ class ScalaRPCClient(instance: BitcoindInstance, username: String, password: Str
    * best block chain. The number is a a multiple of the minimum difficulty
    * https://bitcoin.org/en/developer-reference#getdifficulty
    */
-  def getDifficulty : Double = sendCommand("getdifficulty").trim.toDouble
+  def getDifficulty: Future[Double] = {
+    sendCommand("getdifficulty").map(json => json.fields("result").convertTo[Double])
+  }
 
   /**
    * The getnewaddress RPC returns a new Bitcoin address for receiving payments. If an account is specified,
    * payments received with the address will be credited to that account.
    * https://bitcoin.org/en/developer-reference#getnewaddress
    */
-  def getNewAddress: BitcoinAddress = BitcoinAddress(sendCommand("getnewaddress").trim)
+  def getNewAddress: Future[BitcoinAddress] = {
+    sendCommand("getnewaddress").map(json => BitcoinAddress(json.fields("result").convertTo[String]))
+  }
 
   /**
    * The getrawchangeaddress RPC returns a new Bitcoin address for receiving change. This is for use with raw
    * transactions, not normal use.
    * https://bitcoin.org/en/developer-reference#getrawchangeaddress */
-  def getRawChangeAddress: BitcoinAddress = BitcoinAddress(sendCommand("getrawchangeaddress").trim)
+  def getRawChangeAddress: Future[BitcoinAddress] = {
+    sendCommand("getrawchangeaddress").map(json => BitcoinAddress(json.fields("result").convertTo[String]))
+  }
 
   /**
    * The getbalance RPC gets the balance in decimal bitcoins across all accounts or for a particular account.
    * https://bitcoin.org/en/developer-reference#getbalance */
-  def getBalance : Double = sendCommand("getbalance").toDouble
+  def getBalance: Future[CurrencyUnit] = {
+    sendCommand("getbalance").map(json => doubleToCurrencyUnit(json.fields("result").convertTo[Double]))
+  }
 
   /**
    * The hash of the block header from the most recent block on the best block chain,
    * encoded as hex in RPC byte order
    * https://bitcoin.org/en/developer-reference#getbalance
    */
-  def getBestBlockHash : String = sendCommand("getbestblockhash")
-
-  /**
-    * Given an address, returns a 1-of-1 p2sh address and adds it to the wallet.
-    * https://bitcoin.org/en/developer-reference#addmultisigaddress
-    */
-  def generateOneOfOneMultiSigAddress(sigsRequired : Int, address: String): BitcoinAddress = {
-    //val cmd = "addmultisigaddress " + sigsRequired + " [\\\"" + address + "\\\"]"
-    val addressInJson = "[\"" + address + "\"]"
-    val cmd = "addmultisigaddress 1 " + " " + addressInJson
-    val result : String = sendCommand(cmd)
-    BitcoinAddress(result.trim)
+  def getBestBlockHash: Future[DoubleSha256Digest] = {
+    sendCommand("getbestblockhash").map(json => DoubleSha256Digest(json.fields("result").convertTo[String]))
   }
 
   /** Funds the given transaction with outputs in the bitcoin core wallet
     * [[https://bitcoin.org/en/developer-reference#fundrawtransaction]]
     * */
-  def fundRawTransaction(tx: Transaction): (Transaction, CurrencyUnit, Int) = {
-    val cmd = "fundrawtransaction " + tx.hex
-    val json = sendCommand(cmd).parseJson
-    val f = json.asJsObject.fields
-    val newTx = Transaction(f("hex").convertTo[String])
-    val sat = f("fee").convertTo[Double] * CurrencyUnits.btcToSatoshiScalar
-    require(sat.toLong == sat)
-    val fee = Satoshis(Int64(sat.toLong))
-    val changePosition = f("changepos").convertTo[Int]
-    (newTx,fee,changePosition)
+  def fundRawTransaction(tx: Transaction): Future[(Transaction, CurrencyUnit, Int)] = {
+    val cmd = "fundrawtransaction"
+    sendCommand(cmd,tx.hex).map { json =>
+      val result = json.fields("result")
+      val f = result.asJsObject.fields
+      val newTx = Transaction(f("hex").convertTo[String])
+      val sat = f("fee").convertTo[Double]
+      val fee = doubleToCurrencyUnit(sat)
+      val changePosition = f("changepos").convertTo[Int]
+      (newTx,fee,changePosition)
+    }
   }
 
   /** Signs the given raw transaction
     * [[https://bitcoin.org/en/developer-reference#signrawtransaction]]
     * */
-  def signRawTransaction(tx: Transaction): (Transaction, Boolean) = {
-    val cmd = "signrawtransaction " + tx.hex
-    val json = sendCommand(cmd).parseJson
-    val f = json.asJsObject.fields
-    val signedTx = Transaction(f("hex").convertTo[String])
-    val isComplete = f("complete").convertTo[Boolean]
-    (signedTx,isComplete)
+  def signRawTransaction(tx: Transaction): Future[(Transaction, Boolean)] = {
+    val cmd = "signrawtransaction"
+    sendCommand(cmd,tx.hex).map { json =>
+      val result = json.fields("result")
+      val f = result.asJsObject.fields
+      val signedTx = Transaction(f("result").convertTo[String])
+      val isComplete = f("complete").convertTo[Boolean]
+      (signedTx,isComplete)
+    }
   }
 
-  def listUnspent: Seq[UTXO] = {
+  def listUnspent: Future[Seq[UTXO]] = {
     val cmd = "listunspent"
-    val json = sendCommand(cmd).parseJson
-    val utxos: Seq[UTXO] = json.convertTo[Seq[UTXO]]
-    utxos
+    sendCommand(cmd).map { json =>
+      val utxos: Seq[UTXO] = json.fields("result").convertTo[Seq[UTXO]]
+      utxos
+    }
   }
 
   /**
     * Imports the private key into bitcoin core's wallet
     * [[https://bitcoin.org/en/developer-reference#importprivkey]]
     */
-  def importPrivateKey(privKey: ECPrivateKey): Boolean = {
-    val cmd = "importprivkey " + privKey.toWIF(instance.network)
-    val result = sendCommand(cmd)
-    result.isEmpty
+  def importPrivateKey(privKey: ECPrivateKey): Future[Boolean] = {
+    val key = privKey.toWIF(instance.network)
+    sendCommand("importprivkey",key).map(json => json.fields.keys.isEmpty)
   }
 
   /**
     * Dumps the private key associated with this address
     * [[https://bitcoin.org/en/developer-reference#dumpprivkey]]
     */
-  def dumpPrivateKey(address: P2PKHAddress): ECPrivateKey = {
-    val cmd = "dumpprivkey " + address.value
-    val result = sendCommand(cmd).trim
-    ECPrivateKey.fromWIFToPrivateKey(result)
+  def dumpPrivateKey(address: P2PKHAddress): Future[ECPrivateKey] = {
+    sendCommand("dumpprivkey",address.value).map { json =>
+      ECPrivateKey.fromWIFToPrivateKey(json.fields("result").convertTo[String])
+    }
   }
 
   private def parseNetworkArg: String = {
     if (instance.network == MainNet) "" else "-" + instance.network.name
+  }
+
+  private def doubleToCurrencyUnit(d: Double): CurrencyUnit = {
+    val sat = d * CurrencyUnits.btcToSatoshiScalar
+    require(sat.toLong == sat)
+    Satoshis(Int64(sat.toLong))
   }
 }
 
--- src/test/scala/org/bitcoins/rpc/ScalaRPCClientTest.scala
+++ src/test/scala/org/bitcoins/rpc/ScalaRPCClientTest.scala
@@ -4,31 +4,30 @@ import java.io.PrintWriter
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.model.Uri
+import akka.stream.ActorMaterializer
 import org.bitcoins.core.config.RegTest
 import org.bitcoins.core.crypto.ECPrivateKey
-import org.bitcoins.core.currency.CurrencyUnits
-import org.bitcoins.core.protocol.{Address, BitcoinAddress, P2PKHAddress}
-import org.bitcoins.rpc.marshallers.blockchain.{BlockchainInfoRPCMarshaller, ConfirmedUnspentTransactionOutputMarshaller, MemPoolInfoMarshaller}
-import org.bitcoins.rpc.marshallers.mining.MiningInfoMarshaller
-import org.bitcoins.rpc.marshallers.networking.{NetworkRPCMarshaller, PeerInfoRPCMarshaller}
-import org.bitcoins.rpc.marshallers.wallet.WalletMarshaller
+import org.bitcoins.core.currency.{CurrencyUnits, Satoshis}
+import org.bitcoins.core.number.Int64
+import org.bitcoins.core.protocol.P2PKHAddress
 import org.bitcoins.core.protocol.script.EmptyScriptPubKey
 import org.bitcoins.core.protocol.transaction.{Transaction, TransactionConstants, TransactionOutput}
 import org.bitcoins.core.util.BitcoinSLogger
 import org.bitcoins.rpc.config.BitcoindInstance
+import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{BeforeAndAfterAll, FlatSpec, MustMatchers}
-import org.bitcoins.rpc.marshallers.RPCMarshallerUtil
-import spray.json._
 
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
-import scala.sys.process.Process
 
 /**
   * Created by tom on 4/26/16.
   */
-class ScalaRPCClientTest extends FlatSpec with MustMatchers with BeforeAndAfterAll with BitcoinSLogger {
-  val system = ActorSystem("ScalaRPCClientTest")
+class ScalaRPCClientTest extends FlatSpec with MustMatchers with ScalaFutures with
+  BeforeAndAfterAll with BitcoinSLogger {
+  implicit val actorSystem = ActorSystem("ScalaRPCClientTest")
+  val materializer = ActorMaterializer()
+  implicit val dispatcher = materializer.system.dispatcher
   def randomDirName: String = 0.until(5).map(_ => scala.util.Random.alphanumeric.head).mkString
   val (datadir,username,password) = {
     val d = "/tmp/" + randomDirName
@@ -46,7 +45,7 @@ class ScalaRPCClientTest extends FlatSpec with MustMatchers with BeforeAndAfterA
   }
   val network = RegTest
   val instance = BitcoindInstance(network, Uri("http://localhost:" + network.rpcPort))
-  val test = new ScalaRPCClient(instance,username,password,system,datadir)
+  val test = new ScalaRPCClient(instance,username,password,materializer,datadir)
   //bitcoind -rpcuser=$RPC_USER -rpcpassword=$RPC_PASS -regtest -txindex -daemon
 
   override def beforeAll: Unit = {
@@ -54,94 +53,64 @@ class ScalaRPCClientTest extends FlatSpec with MustMatchers with BeforeAndAfterA
     Thread.sleep(20000)
   }
   "ScalaRPCClient" must "send a command to the command line and return the output" in {
-    test.generate(101)
-    test.getBlockCount must be(101)
-  }
-
-  it must "parse and return networkinfo" in {
-    val networkInfo = test.sendCommand("getnetworkinfo")
-    val json = networkInfo.parseJson
-    test.getNetworkInfo must be (NetworkRPCMarshaller.NetworkInfoFormatter.read(json))
-    test.getNetworkInfo.version must be (NetworkRPCMarshaller.NetworkInfoFormatter.read(json).version)
-  }
-
-  it must "parse and return mininginfo" in {
-    val miningInfo = test.sendCommand("getmininginfo")
-    val json = miningInfo.parseJson
-    test.getMiningInfo must be (MiningInfoMarshaller.MiningInfoFormatter.read(json))
-  }
-
-  it must "parse and return blockchaininfo" in {
-    val blockchainInfo = test.sendCommand("getblockchaininfo")
-    val json = blockchainInfo.parseJson
-    test.getBlockChainInfo must be (BlockchainInfoRPCMarshaller.BlockchainInfoFormatter.read(json))
-    test.getBlockChainInfo.chain must be ("regtest")
-  }
-
-  it must "parse and return peer info" in {
-    val peerInfo = test.sendCommand("getpeerinfo")
-    val json = peerInfo.parseJson
-    test.getPeerInfo must be (test.convertToPeerInfoSeq(json))
-  }
-
-  it must "parse and return mempoolinfo" in {
-    val mempoolInfo = test.sendCommand("getmempoolinfo")
-    val json = mempoolInfo.parseJson
-    test.getMemPoolInfo must be (MemPoolInfoMarshaller.MemPoolInfoFormatter.read(json))
-  }
-
-  it must "parse and return txoutset info" in {
-    val txOutSetInfo = test.sendCommand("gettxoutsetinfo")
-    val json = txOutSetInfo.parseJson
-    test.getTxOutSetInfo must be {
-      ConfirmedUnspentTransactionOutputMarshaller.ConfirmedUnspentTransactionOutputFormatter.read(json)
+    val hashes = test.generate(101)
+    val blockCount = hashes.flatMap(_ => test.getBlockCount)
+    whenReady(blockCount, timeout(5.seconds), interval(500.millis)) { count =>
+      count must be (101)
     }
   }
 
-  it must "parse and return wallet info" in {
-    val walletInfo = test.sendCommand("getwalletinfo")
-    val json = walletInfo.parseJson
-    test.getWalletInfo must be (WalletMarshaller.WalletFormatter.read(json))
-  }
-
   it must "get difficuluty" in {
-    test.getDifficulty must be (4.656542373906925E-10)
+    whenReady(test.getDifficulty, timeout(5.seconds), interval(500.millis)) { diff =>
+      diff must be (4.656542373906925E-10)
+    }
   }
 
   it must "get new address" in {
-    test.getNewAddress.isInstanceOf[BitcoinAddress] must be (true)
+    whenReady(test.getNewAddress, timeout(5.seconds), interval(500.millis)) { addr =>
+      //just make sure we can parse it
+    }
   }
 
   it must "get raw change address" in {
-    test.getRawChangeAddress.isInstanceOf[BitcoinAddress] must be (true)
+    whenReady(test.getRawChangeAddress, timeout(5.seconds), interval(500.millis)) { addr =>
+      //just make sure we can parse it
+    }
   }
 
   it must "get the balance" in {
-    test.getBalance must be (50.0)
+    whenReady(test.getBalance, timeout(5.seconds), interval(500.millis)) { satoshis =>
+      satoshis must be (Satoshis(Int64(50L * CurrencyUnits.btcToSatoshiScalar)))
+    }
   }
 
   it must "list utxos" in {
-    test.listUnspent.nonEmpty must be (true)
+    whenReady(test.listUnspent, timeout(5.seconds), interval(500.millis)) { utxos =>
+      utxos.nonEmpty must be (true)
+    }
   }
 
   it must "fund a raw transaction" in {
     val output = TransactionOutput(CurrencyUnits.oneBTC,EmptyScriptPubKey)
     val unfunded = Transaction(TransactionConstants.version,Nil,Seq(output),TransactionConstants.lockTime)
-    val (tx,fees,changepos) = test.fundRawTransaction(unfunded)
-    tx.inputs.nonEmpty must be (true)
+    whenReady(test.fundRawTransaction(unfunded), timeout(5.seconds), interval(500.millis)) { case (tx,fee,changepos) =>
+      tx.inputs.nonEmpty must be (true)
+    }
+
   }
 
   it must "be able to import a private key and then dump it" in {
     val key = ECPrivateKey()
-    val imp = test.importPrivateKey(key)
-    imp must be (true)
     val address = P2PKHAddress(key.publicKey,network)
-    val dumpedKey = test.dumpPrivateKey(address)
-    dumpedKey must be (key)
+    val imp = test.importPrivateKey(key)
+    val dumpedKeyFuture = imp.flatMap(_ => test.dumpPrivateKey(address))
+    whenReady(dumpedKeyFuture, timeout(5.seconds), interval(500.millis)) { dumpedKey =>
+      dumpedKey must be (key)
+    }
   }
 
   override def afterAll = {
-    system.terminate()
+    materializer.shutdown()
     Await.result(test.stop,5.seconds)
   }
 }
