d6f645f02cb89e5ab8709959109e219a0c6a45e9
Chris Stewart <stewart.chris1234@gmail.com>
2017-05-08 12:14:31 -0500

    Refactoring RPCPaymentChannelTest to connect two nodes

--- src/test/scala/org/bitcoins/rpc/RPCPaymentChannelTest.scala
+++ src/test/scala/org/bitcoins/rpc/RPCPaymentChannelTest.scala
@@ -17,17 +17,23 @@ import scala.concurrent.duration.DurationInt
   * Created by chris on 5/3/17.
   */
 class RPCPaymentChannelTest extends FlatSpec with MustMatchers with ScalaFutures with BeforeAndAfterAll {
-/*
+
   implicit val actorSystem = ActorSystem("RPCClientTest")
   val materializer = ActorMaterializer()
   implicit val dispatcher = materializer.system.dispatcher
-  val test = RPCClient(TestUtil.instance,materializer)
+  val node1 = TestUtil.instance(TestUtil.network.port - 15, TestUtil.network.rpcPort - 15)
+  val client1 = RPCClient(node1,materializer)
+  val node2 = TestUtil.instance(TestUtil.network.port + 15, TestUtil.network.rpcPort + 15)
+  val client2 = RPCClient(node2,materializer)
   //bitcoind -rpcuser=$RPC_USER -rpcpassword=$RPC_PASS -regtest -txindex -daemon
 
   override def beforeAll: Unit = {
-    test.start
-    Thread.sleep(20000)
-    Await.result(test.generate(101),5.seconds)
+    TestUtil.startNodes(Seq(client1,client2))
+    val connected = client1.addNode(client1.instance.uri).map { _ =>
+      client2.addNode(client2.instance.uri)
+    }
+    val generateBlocks = connected.flatMap(_ => client1.generate(101))
+    Await.result(generateBlocks,5.seconds)
   }
 
   "RPCPaymentChannels" must "create a payment channel from the client's perspective" in {
@@ -38,14 +44,14 @@ class RPCPaymentChannelTest extends FlatSpec with MustMatchers with ScalaFutures
     val scriptPubKey = EscrowTimeoutScriptPubKey(multiSig,lockTimeScriptPubKey)
     val output = TransactionOutput(CurrencyUnits.oneBTC, scriptPubKey)
     val unfunded = Transaction(TransactionConstants.version,Nil,Seq(output), TransactionConstants.lockTime)
-    val fundedTx: Future[(Transaction, CurrencyUnit, Int)] = test.fundRawTransaction(unfunded)
-    val signedTx = fundedTx.flatMap(f => test.signRawTransaction(f._1))
-    val sent = signedTx.flatMap(tx => test.sendRawTransaction(tx._1))
+    val fundedTx: Future[(Transaction, CurrencyUnit, Int)] = client1.fundRawTransaction(unfunded)
+    val signedTx = fundedTx.flatMap(f => client1.signRawTransaction(f._1))
+    val sent = signedTx.flatMap(tx => client1.sendRawTransaction(tx._1))
 
   }
 
   override def afterAll = {
     materializer.shutdown()
-    Await.result(test.stop,5.seconds)
-  }*/
+    Await.result(TestUtil.stopNodes(Seq(client1,client2)),5.seconds)
+  }
 }
